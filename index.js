9269530e5e6d8a6b6192253c4c9f7c05ea110237
Nikhil Singh <nikhilsingh8420@gmail.com>
2023-07-10 22:29:10 +0530

    Added comment in index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-
-
+// Added comment
 var randomNumber1 = Math.floor(Math.random() * 6) + 1 ;
 var randomDiceImage = "dice" + randomNumber1 + ".png";
 var randomImageSource = "images/" + randomDiceImage;
